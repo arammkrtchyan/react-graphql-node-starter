c8aaf784ae63f4c612d46ba4cd7ea85bd1ef5bdd
gg_aram <aram@greatergood.com>
2018-06-19 15:20:57 +0400

    Inital setup of react web application.

---
+++ web/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(
+  <span>Sample Application</span>,
+  document.getElementById("root")
+);
+registerServiceWorker();
