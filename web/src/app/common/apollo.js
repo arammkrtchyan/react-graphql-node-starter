4c20572e1f3b011b8510700f9d77a080e839056a
gg_aram <aram@greatergood.com>
2018-06-20 11:57:57 +0400

    add error link to apollo client.

--- web/src/app/common/apollo.js
+++ web/src/app/common/apollo.js
@@ -1,6 +1,8 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createHttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
 import ApolloClient from "apollo-client";
+import { ApolloLink } from "apollo-link";
 
 const env = process.env;
 
@@ -10,7 +12,9 @@ const apiPort =
     ? ``
     : `:${env.REACT_APP_API_PORT}`;
 
-const apiUrl = `${env.REACT_APP_HTTP_PROTOCOL}://${env.REACT_APP_API_HOST}${apiPort}/api`;
+const apiUrl = `${env.REACT_APP_HTTP_PROTOCOL}://${
+  env.REACT_APP_API_HOST
+}${apiPort}/api`;
 
 console.log(apiUrl);
 
@@ -18,7 +22,19 @@ const cache = new InMemoryCache({
   dataIdFromObject: object => object.id
 });
 
+export const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, location, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${location}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const links = [errorLink, createHttpLink({ uri: `/api/graphql` })];
+
 export const client = new ApolloClient({
-  link: createHttpLink({ uri: `/api/graphql` }),
+  link: ApolloLink.from(links),
   cache
 });
